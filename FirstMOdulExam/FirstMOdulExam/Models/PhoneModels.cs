daae2ee633aea09d670f9591760ef78c2dbd1a33
pirmuhammedov <muhammaddiyor0620@gmail.com>
2024-11-30 10:53:50 +0300

    First Modul Exam

---
+++ FirstMOdulExam/FirstMOdulExam/Models/PhoneModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMOdulExam.Models;
+
+internal class PhoneModels
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public double Price { get; set; }
+    public string Color { get; set; }
+
+}
--- FirstMOdulExam/FirstMOdulExam/Program.cs
+++ FirstMOdulExam/FirstMOdulExam/Program.cs
@@ -6,5 +6,51 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        //1__
+        public static int EvenNumbers(List<int> numbers)
+        {
+            var evens = 0;
+            List<int> list = new List<int>();
+            foreach (int i in list)
+            {
+                if (i % 2 == 0)
+                {
+                    evens++;
+                }
+            }
+            return evens;
+        }
+
+        // 2__
+        public static bool ElementIsBiggerThanFive(List<string> texts)
+        {
+            foreach (string text in texts)
+            {
+                if (text.Length > 5)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // 3__
+        public static string FirstThreeWords(string text)
+        {
+            text = text.Substring(0, 3);
+            return text;
+        }
+
+        // 4__
+        public static bool FirstWordIsPDP(string word)
+        {
+            var text = word.Substring(0, 3);
+            if (text == "PDP")
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
